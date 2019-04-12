0d533503461849547d0cb4c3a1d88d7d02b7a59d
JULIO <juliolotr@gmail.com>
2019-04-11 22:15:41 -0400

    second commit

--- src/test/java/org/umssdiplo/automationv01/stepdefinitionproject/StepsDefinitionPHPtravel.java
+++ src/test/java/org/umssdiplo/automationv01/stepdefinitionproject/StepsDefinitionPHPtravel.java
@@ -1,6 +1,5 @@
 package org.umssdiplo.automationv01.stepdefinitionproject;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import org.umssdiplo.automationv01.core.managepage.Login.Login;
@@ -8,6 +7,7 @@ import org.umssdiplo.automationv01.core.utils.LoadPage;
 
 public class StepsDefinitionPHPtravel {
     private Login login;
+    private org.umssdiplo.automationv01.core.managepage.catalogo.clickCatalogo clickCatalogo;
 
     @Given("^'PHP travel' page is loaded$")
     public void phpTravelPageIsLoaded() throws Throwable {
@@ -17,5 +17,15 @@ public class StepsDefinitionPHPtravel {
     @And("^set my credentials on 'Login' page$")
     public void setMyCredentialsOnLoginPage() throws Throwable {
         login.setCredentials();
+    }
+
+    @Given("^'Tractorstore' page in 'header' page$")
+    public void tractorstorePageInHeaderPage() {
+        clickCatalogo = LoadPage.catalogoPage();
+    }
+
+    @And("^click 'Catalogo' page in 'buton'$")
+    public void clickCatalogoPageInButon() {
+        clickCatalogo.clickButtonH();
     }
 }
